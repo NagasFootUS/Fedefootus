b3c0266081710b49deef6fa7a5ebc40ec43585bb
NagasFootUS <61389907+NagasFootUS@users.noreply.github.com>
2020-06-09 15:57:58 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>FFSU American Football</title>
+  </head>
+  <body>
+    <header class="clearfix">
+      <a href="index.html" class="header-home">FFSU American Football</a>
+      <nav>
+        <ul>
+           <li><a>La Ligue Universitaire</a>
+                <ul>
+                    <li><a href="histoire.html">L'Histoire</a></li>
+                    <li><a href="schedule.html">Calendrier 2020</a></li>
+                    <li><a href="result.html">Classement 2020</a></li>
+                </ul>
+            </li>
+            <li><a href="teams.html">Teams</a>
+                <ul>
+                    <li><a href="bears.html">Bears</a></li>
+                    <li><a href="castors.html">Castors</a></li>
+                    <li><a href="crocos.html">Crocos</a></li>
+                    <li><a href="eagles.html">Eagles</a></li>
+                    <li><a href="nagas.html">Nagas</a></li>
+                    <li><a href="outlaws.html">Outlaws</a></li>
+                    <li><a href="raptors.html">Raptors</a></li>
+                    <li><a href="rockets.html">Rockets</a></li>
+                    <li><a href="snakes.html">Snakes</a></li>
+                    <li><a href="stunners.html">Stunners</a></li>
+                </ul>
+            </li>
+            <li><a>All Star Game</a>
+                <ul>
+                    <li><a href="nord.html">Les Teams</a></li>
+                    <li><a href="players.html">La Sélection</a></li>
+                    <li><a href="combine.html">Combine</a></li>
+                    <li><a href="game.html">Le Match</a></li>
+                  </ul>
+            </li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <a href="https://sport-u.com/" class="header-link">FFSU</a>
+      </nav>
+    </header>
+    <main>
+      <div class="container">
+        <div class="contact-box">
+          <div class="left"></div>
+          <div class="right">
+            <h2 class="contacth2">Contact Us</h2>
+            <input type="text" name="name" class="field" placeholder="Votre Nom">
+            <input type="text" name="mail" class="field" placeholder="Votre Email">
+            <input type="text" name="subject" class="field" placeholder="Sujet">
+            <textarea name="message" laceholder="Message" class="field"></textarea>
+            <button name="submit" type="submit" class="btn">Send</button>
+          </div>
+        </div>
+      </div>
+      </main>
+      <footer>
+        <div class="wrapper">
+          <div class="footer">
+            <div class="footerffsu">
+              <img src="ffsubanner.png" alt="ffsubanner">
+            </div>
+            <div class="minilogobanner">
+              <img src="ffsulogo.jpg" alt="minilogo">
+            </div>
+            <div class="footer-sm">
+              <a class="fblogo" href="https://www.facebook.com/ligue.U.FFA/" target="_blank">
+                <img src="facebook.png" alt="facebook logo">
+              </a>
+              <a href="https://www.instagram.com/ligueuniversitairefootball/" target="_blank">
+                <img class="instalogo" src="instgram.png" alt="instagram logo">
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+  </body>
+</html>
